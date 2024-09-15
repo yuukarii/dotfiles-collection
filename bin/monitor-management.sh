8e8a3b7e3efcaee5b94dcdc9904901ac7e95fce9
yuukarii <48701900+yuukarii@users.noreply.github.com>
2024-09-15 21:18:22 +0700

    Add primary monitor and add key binding to pull a container to primary monitor

--- bin/monitor-management.sh
+++ bin/monitor-management.sh
@@ -2,8 +2,9 @@
 monitor_info=$(xrandr | grep connected | grep -v disconnected)
 
 if echo "$monitor_info" | grep -q "HDMI-1"; then
-    echo "HDMI-1 found. Switch to HDMI-1"
-    xrandr --output eDP-1 --auto --output HDMI-1 --auto --right-of eDP-1
+  echo "HDMI-1 found. Switch to HDMI-1"
+  xrandr --output eDP-1 --auto --primary
+  xrandr --output HDMI-1 --auto --right-of eDP-1
 else
-    xrandr --output eDP-1 --auto
+  xrandr --output eDP-1 --auto
 fi
