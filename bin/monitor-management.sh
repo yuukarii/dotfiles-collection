a246808f55d58c85e97344ecc54cc90e529fef95
yuukarii <48701900+yuukarii@users.noreply.github.com>
2024-09-30 18:07:35 +0700

    Remove some files

--- bin/monitor-management.sh
+++ bin/monitor-management.sh
@@ -3,8 +3,8 @@ monitor_info=$(xrandr | grep connected | grep -v disconnected)
 
 if echo "$monitor_info" | grep -q "HDMI-1"; then
   echo "HDMI-1 found. Switch to HDMI-1"
-  xrandr --output eDP-1 --off
   xrandr --output HDMI-1 --auto --primary
+  xrandr --output eDP-1 --off
 else
   xrandr --output eDP-1 --auto --primary
   xrandr --output HDMI-1 --off
